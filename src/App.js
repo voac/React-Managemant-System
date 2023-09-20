69a6fb6f41bb7f8bdb941bcbda3768962f340ea0
v_oac <janghnbn@gmail.com>
2023-09-20 20:03:42 +0900

    nice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
 
-const customer = {
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://random.imagecdn.app/64/64',
   'name': '홍길동',
   'birthday': '961122',
   'gender': '남자',
   'job': '대학생'
-}
+  },
+  {
+    'id': 2,
+    'image': 'https://random.imagecdn.app/64/64',
+    'name': '장현빈',
+    'birthday': '061123',
+    'gender': '남자',
+    'job': '고등학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://random.imagecdn.app/64/64',
+    'name': '응애',
+    'birthday': '061111',
+    'gender': '남자',
+    'job': '배우'
+  }
+]
+
 function App() {
   return (
-    <Customer
-      name = {customer.name}
-      birthday = {customer.birthday}
-      gender = {customer.gender}
-      job = {customer.job}
-    />
+    <div>
+      // map을 사용할 땐 key 라는 이름의 props를 사용하자.
+      { customers.map(c => { return <Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job} /> }) }
+    </div>
   );
 }
 
